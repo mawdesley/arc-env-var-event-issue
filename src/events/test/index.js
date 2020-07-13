e7d5473fd901a27a1e130727b116c74d44010222
Chris Mawdesley <chris.mawdesley@gluckgames.com>
2020-07-13 10:24:18 +0100

    print env var from http lambda

--- src/events/test/index.js
+++ src/events/test/index.js
@@ -1,5 +1,5 @@
 // learn more about event functions here: https://arc.codes/primitives/events
 exports.handler = async function subscribe (event) {
-  console.log(`FOO IS ${process.env.FOO}`);
+  console.log(`In event lambda: FOO IS ${process.env.FOO}`);
   return
 }
